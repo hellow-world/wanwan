5e241fcf64f9783db50790c3514ee54534005382
John <mowanbin18@163.com>
2018-04-13 11:58:56 +0800

    修复活动添加时的动态提示以及权限设置时的动态操作

--- admin-wanwan/1.3.2/src/page/activity/activity-add/index.js
+++ admin-wanwan/1.3.2/src/page/activity/activity-add/index.js
@@ -2,7 +2,7 @@
  * @Author: admin
  * @Date:   2018-02-05 10:02:49
  * @Last Modified by:   John
- * @Last Modified time: 2018-04-13 09:51:33
+ * @Last Modified time: 2018-04-13 11:45:46
  */
 require('./index.css')
 var config = require('service/config.js')
@@ -16,19 +16,18 @@ var roleList_temp
 var roleConfig = new Array(); //单个角色配置
 var roleSingle;
 roleSingle = {
-    'roleId': 0, //角色ID
-    'enrolStartTime': '2018-12-20', //报名开始时间
-    'enrolEndTime': '2018-12-20', //报名结束时间
-    'carryNumber': 0, //可携带人数
-    'enrolCurrency': 1, //支付方式
+
+    'roleId'          : 0, //角色ID
+    'enrolStartTime'  : '2018-12-20', //报名开始时间
+    'enrolEndTime'    : '2018-12-20', //报名结束时间
+    'carryNumber'     : 0, //可携带人数
+    'enrolCurrency'   : 1, //支付方式
     'enrolCurrencyNum': 100,
-    'enrolMaxPeople': 10, //报名最大人数
-    'isPush': 0, //是否推送
-    'pushContent': '推送推送推送他',
-    'pushTime': '2018-12-20',
-    'roleName': '随国林'
-
-
+    'enrolMaxPeople'  : 10, //报名最大人数
+    'isPush'          : 0, //是否推送
+    'pushContent'     : '推送推送推送他',
+    'pushTime'        : '2018-12-20',
+    'roleName'        : '随国林'
 }
 $(function() {
     $('#activity_content').froalaEditor({
@@ -107,7 +106,7 @@ function addSelectToBox(param) {
 // 为商家分组赋值
 
 function setSellerSelect(param) {
-    let res = ``;
+    let res = `<option value="1" data-seller="-1">请选择活动发布者</option>`;
     for (var i = 0; i < param.length; i++) {
 
         res += addSelectToBoxSeller(param[i], i)
@@ -181,57 +180,68 @@ function getNowFormatDate() {
 $('#addSubmit').click(function() { addActivitySubmit(); })
 
 function addActivitySubmit() {
-    var activity_title = $('#activity_title').val();
-    var activity_introduce = $('#activity_introduce').val();
-    var activity_place = $('#activity_place').val();
-    var Issuer = $('.sellerUser').find('option:selected').attr('data-seller')
+    var activity_title         = $('#activity_title').val();
+    var activity_introduce     = $('#activity_introduce').val();
+    var activity_place         = $('#activity_place').val();
+    var Issuer                 = $('.sellerUser').find('option:selected').attr('data-seller')
     var activity_maxnum_people = $('#activity_maxnum_people').val();
-    var activity_start_time = $('#activity_start_time').val();
-    var activity_end_time = $('#activity_end_time').val();
-    var activity_content = $('div#activity_content').froalaEditor('html.get');
-    var s = activity_content.replace(/style\s*=(['\"\s]?)[^'\"]*?\1/gi, '');
-    var s = s.replace(/width\s*:(['\"\s]?)[^'\"]*?\1/gi, '');
+    var activity_start_time    = $('#activity_start_time').val();
+    var activity_end_time      = $('#activity_end_time').val();
+    var isonline               = $('input[name=isOnline]:checked').val();
+    var activity_online_time   = $('#activity_online_time').val();
+    var activity_content       = $('div#activity_content').froalaEditor('html.get');
+    var s                      = activity_content.replace(/style\s*=(['\"\s]?)[^'\"]*?\1/gi, '');
+    var s                      = s.replace(/width\s*:(['\"\s]?)[^'\"]*?\1/gi, '');
 
     var activity_CreateTime = getNowFormatDate();
-    // if (activity_title == "") {
-    //     $("#Tip").html("活动标题必填");
-    // } else if (document.getElementById("titlePic").value == "") {
-    //     $("#Tip").html("请上传正确格式的标题图片");
-    // } else if (document.getElementById("coverPic").value == "") {
-    //     $("#Tip").html("请上传正确格式的封面图片");
-    // } else if (activity_introduce == "") {
-    //     $('#activity_introduce').val('无');
-    // } else if (activity_place == "") {
-    //     $("#Tip").html("活动地点必填");
-    // } else if (enrol_currency == "") {
-    //     $("#Tip").html("获取类型必填");
-    // } else if (activity_number_people == "") {
-    //     $("#Tip").html("人数上限必填");
-    // } else if (activity_start_time == "" || activity_end_time == "") {
-    //     $("#Tip").html("开始时间必填");
-    // } else if (enrol_start_time == "" || enrol_end_time == "") {
-    //     $("#Tip").html("报名时间必填");
-    // } else {
+    if (activity_title == "") {
+        $("#Tip").html("活动名称必填");
+    } else if (document.getElementById("titlePic").value == "") {
+        $("#Tip").html("请上传正确格式的标题图片");
+    } else if (document.getElementById("coverPic").value == "") {
+        $("#Tip").html("请上传正确格式的封面图片");
+    } else if (activity_introduce == "") {
+        $('#activity_introduce').val('无');
+    } else if (activity_place == "") {
+        $("#Tip").html("活动地点必填");
+    } else if (activity_number_people == "") {
+        $("#Tip").html("总人数上限必填");
+    } else if (activity_start_time == "" || activity_end_time == "") {
+        $("#Tip").html("开始时间必填");
+    } else if(Issuer == -1){   
+        $("#Tip").html("发布商家必选");
+    } else if(roleConfig.length < 1){   
+        $("#Tip").html("权限分组未设置");
+    } else {
 
         var formData = new FormData();
-        formData.append("activityTitle", activity_title);
-        formData.append("activityIntroduce", activity_introduce);
-        formData.append("activityPlace", activity_place);
-        formData.append("activityContent", s);
-        formData.append("activityTitlePhoto", document.getElementById("titlePic").files[0]);
-        formData.append("activityCover", document.getElementById("coverPic").files[0]);
+        formData.append("activityTitle", activity_title);//活动标题
+        formData.append("activityIntroduce", activity_introduce);//活动简介
+        formData.append("activityPlace", activity_place);//活动地点
+        formData.append("activityContent", s);//活动内容
+        formData.append("activityTitlePhoto", document.getElementById("titlePic").files[0]);//活动标题图片
+        formData.append("activityCover", document.getElementById("coverPic").files[0]);//活动封面图片
 
-        formData.append("activityMaxPeople", activity_maxnum_people);
-        formData.append("activityStartTime", activity_start_time);
-        formData.append("activityEndTime", activity_end_time);
+        formData.append("activityMaxPeople", activity_maxnum_people);//活动最大人数限制
+        formData.append("activityStartTime", activity_start_time);//活动开始时间
+        formData.append("activityEndTime", activity_end_time);//活动结束时间
         
-        formData.append("activityCreateTime", activity_CreateTime);
-        formData.append("activityIssuer",Issuer)
+        if(activity_online_time == "")
+        {
+            formData.append("activityOnlineTime", activity_CreateTime);//活动上架时间
+        }
+        else
+        {
+            formData.append("activityOnlineTime", activity_online_time);//活动上架时间
+        }
+        
+        formData.append("activityIssuer", Issuer)//活动发布者
 
 
-        formData.append("isOnline",1);
+        formData.append("isOnline", isonline);//活动是否上架
         //角色配置
-        formData.append("roleConfig",JSON.stringify(roleConfig));
+        formData.append("roleConfig", JSON.stringify(roleConfig));//活动权限角色配置
+
         $.ajax({
             url: config.buildPath + "api-amuse/v1.1/activity",
             type: "POST",
@@ -256,6 +266,7 @@ function addActivitySubmit() {
                 alert(a.status);
             }
         });
+    }
 
 }
 
@@ -271,8 +282,21 @@ function backToList() {
 window.selectPowerUser = (obj) => {
     let roleId = $(obj).parent('label').attr('data-role');
     if ($(obj).parent('label').hasClass('powerOn')) {
-        $(obj).parent('label').removeClass('powerOn');
-        return;
+        if(PowerIsSet(roleId))
+        {
+            //清空该权限的设置
+
+            _utils.modalalert('该权限设置已清空');
+            $(obj).parent('label').removeClass('powerOn');
+            $(obj).siblings('.powerSet').html('设置')
+            return;
+        }
+        else
+        {
+            $(obj).parent('label').removeClass('powerOn');
+            return;
+        }
+        
     }
     if (roleId == -1) {
         $('.powerBlock').find('label').removeClass('powerOn');
@@ -283,6 +307,26 @@ window.selectPowerUser = (obj) => {
     }
 
 }
+// 确定要清空
+var PowerResetconfirm = (bool)=>
+{
+    if( bool == 1 )
+    {
+        return true;
+    }
+}
+//判断权限设置切换选择时是否已在序列
+var PowerIsSet = (id)=>
+{
+    for (var i = 0; i < roleConfig.length; i++) {
+        if(id == roleConfig[i].roleId)
+        {
+            roleConfig.splice(i)
+            return true;
+        }
+    }
+}
+//设置权限
 window.setPowerUser = (obj) => {
 
     if (!$(obj).parent('label').hasClass('powerOn')) {
@@ -297,18 +341,44 @@ window.setPowerUser = (obj) => {
     $('#addSetPower').find('#power-input-name').val(powerName);
     $('#addSetPower').find('#power-input-name').attr('data-roleid', powerId);
 
-    
+
 }
+//设置权限存入暂存数列
 window.setPowerSubmit = () => {
-    let roleId = $('#addSetPower').find('#power-input-name').attr('data-roleid');
-    roleSingle.roleId = $('#addSetPower').find('#power-input-name').attr('data-roleid');
-    roleSingle.roleName = $('#addSetPower').find('#power-input-name').val();
-    roleSingle.enrolStartTime = $('#addSetPower').find('#enrol_start_time').val();
-    roleSingle.enrolEndTime = $('#addSetPower').find('#enrol_end_time').val();
-    roleSingle.carryNumber = $('#addSetPower').find('#activity_carrynumber_people').val();
-    roleSingle.enrolCurrency = $('#addSetPower').find('#enrol_currency').val();
+    if($('#addSetPower').find('#enrol_start_time').val() == '')
+    {
+        _utils.modalalert('报名时间未填写')
+        return;
+    }
+    else if($('#addSetPower').find('#activity_carrynumber_people').val() == '')
+    {
+        _utils.modalalert('携带人数未填写')
+        return;
+    }
+    else if($('#addSetPower').find('#enrol_currency').val() == -1)
+    {
+        _utils.modalalert('支付方式未选择')
+        return;
+    }
+    else if($('#addSetPower').find('#money_number').val() == '')
+    {
+        _utils.modalalert('货币数量未填写')
+        return;
+    }
+    else if($('#addSetPower').find('#activity_number_people').val() == '')
+    {
+        _utils.modalalert('人数上限未填写')
+        return;
+    }
+    let roleId                  = $('#addSetPower').find('#power-input-name').attr('data-roleid');
+    roleSingle.roleId           = $('#addSetPower').find('#power-input-name').attr('data-roleid');
+    roleSingle.roleName         = $('#addSetPower').find('#power-input-name').val();
+    roleSingle.enrolStartTime   = $('#addSetPower').find('#enrol_start_time').val();
+    roleSingle.enrolEndTime     = $('#addSetPower').find('#enrol_end_time').val();
+    roleSingle.carryNumber      = $('#addSetPower').find('#activity_carrynumber_people').val();
+    roleSingle.enrolCurrency    = $('#addSetPower').find('#enrol_currency').val();
     roleSingle.enrolCurrencyNum = $('#addSetPower').find('#money_number').val();
-    roleSingle.enrolMaxPeople = $('#addSetPower').find('#activity_number_people').val();
+    roleSingle.enrolMaxPeople   = $('#addSetPower').find('#activity_number_people').val();
     if (isPushed == 1) {
         notifyContent = $('#addSetPower').find("#notify_content").val();
         notifyTime = $('#addSetPower').find("#push_time").val();
@@ -320,37 +390,36 @@ window.setPowerSubmit = () => {
     roleConfig.push(roleSingle);
     console.log(roleConfig)
     console.log(typeof roleConfig)
-    _utils.modalalert('设置成功',500)
+    _utils.modalalert('设置成功', 500)
     $('#addSetPower').modal('hide');
     tabSetState(roleId);
 
 }
-var tabSetState = (id)=>
-{
+//切换权限已设置与未设置状态
+var tabSetState = (id) => {
     let role = $('.powerBox').find('li')
     for (var i = 0; i < role.length; i++) {
-        if($(role[i]).find('label').attr('data-role') == id)
-        {
+        if ($(role[i]).find('label').attr('data-role') == id) {
             $(role[i]).find('.powerSet').text('已设置')
         }
-        
+
     }
 
 }
-var roleSingleInit = ()=>
-{
+//初始化权限设置序列
+var roleSingleInit = () => {
     roleSingle = {
-        'roleId': 0, //角色ID
-        'enrolStartTime': '2018-12-20', //报名开始时间
-        'enrolEndTime': '2018-12-20', //报名结束时间
-        'carryNumber': 0, //可携带人数
-        'enrolCurrency': 1, //支付方式
+        'roleId'          : 0, //角色ID
+        'enrolStartTime'  : '2018-12-20', //报名开始时间
+        'enrolEndTime'    : '2018-12-20', //报名结束时间
+        'carryNumber'     : 0, //可携带人数
+        'enrolCurrency'   : 1, //支付方式
         'enrolCurrencyNum': 100,
-        'enrolMaxPeople': 10, //报名最大人数
-        'isPush': 0, //是否推送
-        'pushContent': '推送推送推送他',
-        'pushTime': '2018-12-20',
-        'roleName': '随国林'
+        'enrolMaxPeople'  : 10, //报名最大人数
+        'isPush'          : 0, //是否推送
+        'pushContent'     : '推送推送推送他',
+        'pushTime'        : '2018-12-20',
+        'roleName'        : '随国林'
 
     }
     $('#addSetPower').find('#power-input-name').val('');
